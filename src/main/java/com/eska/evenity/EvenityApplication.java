a08a28c29b18a3fad55178fb9e6aaa25bafea83c
umifarida98 <umifarida98@gmail.com>
2024-10-30 08:40:10 +0700

    first commit

---
+++ src/main/java/com/eska/evenity/EvenityApplication.java
@@ -0,0 +1,13 @@
+package com.eska.evenity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eska/evenity/EvenityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eska.evenity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
